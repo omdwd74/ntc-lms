52294c38752da31c3667dd92fc244ae7de34eb4f
omdwd74 <omjidwivedi123@gmail.com>
2024-01-12 16:35:17 +0530

    update backend

--- backend/src/models/book.js
+++ backend/src/models/book.js
@@ -3,11 +3,15 @@ const { model, Schema } = require("mongoose")
 const BookModel = model(
   "books",
   new Schema({
-    name: { type: String, required: true },
+    accNo: {type: Number, required: true },
+    title: { type: String, required: true },
+    edition: { type: String, required: true },
+    publisher: { type: String, required: true  },
     keyw: { type:String, required:true},
     isbn: { type: String, required: true, unique: true },
     category: { type: String, required: true },
     price: { type: Number, required: true },
+    tob: {type: String, required: true },
     quantity: { type: Number, required: true },
     authorName: { type: String, required: true},
     borrowedBy: [{ type: Schema.Types.ObjectId, ref: "users" }],
