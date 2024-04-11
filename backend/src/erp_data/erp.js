d2aa3bf4b9bafd8247643799cb06dcc39e552d1e
omdwd74 <omjidwivedi123@gmail.com>
2024-04-11 17:00:57 +0530

    updated collections

---
+++ backend/src/erp_data/erp.js
@@ -0,0 +1,25 @@
+module.exports = [
+    {
+        accNo:95665565,
+        title:"jiojijiijiji",
+        edition:"villio",
+        publisher:"tripathi",
+        keyw:"werrt",
+        isbn:9856,
+        category:"gener",
+        price:899,
+        tob:"spiral",
+        quantity:56,
+        authorName:"Zomato",
+        nop:5999,
+        vol:"IIT",
+        supname:"MK Tomar",
+        supplc:"third floor",
+        curr:"$$3",
+        disc:0,
+        dte:"13april2024",
+        borrowedBy:[],
+        priceHistory:[],
+        quantityHistory:[]
+    }
+]
